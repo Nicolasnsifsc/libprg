cc4af281e58dd7d23a4fc26e46708b632932cb50
nicolasnsifsc <nicolas.ns02@aluno.ifsc.edu.br>
2023-10-30 11:18:32 -0300

    Merge_sort.c adicionado 30 de outubro de 2023

---
+++ libprg/src/libprg/Merge_sort.c
@@ -0,0 +1,28 @@
+//
+// Created by aluno on 30/10/23.
+//
+#include <stdio.h>
+
+// Em C esquerda começa com 0 e direita com n − 1
+int merge_sort(int *vetor[10], int esquerda, int direita)
+{
+    int meio = 0;
+  if (esquerda < direita) {
+      meio = esquerda + (direita − esquerda)/2; // divisão
+      ordene as duas metades(conquista);
+      merge_sort(vetor, esquerda, meio);
+      merge_sort(vetor, meio + 1, direita);
+      // mescle as metades (combinação)
+      merge(vetor, esquerda, meio, direita);
+  }
+ retorna (vetor );
+}
+
+
+int main(void)
+{
+    int meio, esquerda, direita, *vetor[10]={0};
+
+
+    return 0;
+}
